2031f782c4258929326817041f29ecb9eaa9e338
Deniel Forir <Deniel@Deniels-MacBook-Pro.local>
2018-05-25 22:34:16 -0400

    homework_4

---
+++ Day_1/Assignment 4/Assignment_4.cs
@@ -0,0 +1,48 @@
+namespace ZombieReport.CommandLine
+{
+    using System;
+    using System.Collections.Generic;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int[] statuses = {1, 2, 3, 4, 5, 6};
+            string[] names = {"Bob", "Tim", "Tom", "Barb", "Kent", "Carlos"};
+            List<string> statusDescriptions = new List<string>();
+
+            for(counter = 0; counter < statuses.Length; counter++)
+            {
+                string status = GetStatusDescription(statuses[counter], names[counter]);
+                statusDescriptions.Add(status);
+                WriteStatus(status);
+            }
+        }
+
+        private static void WriteStatus(string status)
+        {
+            Console.WriteLine(status);
+        }
+
+        private static string GetStatusDescription(int status, string name)
+        {
+            string statusDescription = string.Empty;
+            switch (status)
+            {
+                case 1:
+                    statusDescription = "Status: Alive";
+                    break;
+                case 2:
+                    statusDescription = "Status: Zombie";
+                    break;
+                case 3:
+                    statusDescription = "Status: Dead";
+                    break;
+                case 4:
+                    statusDescription = "Status: Unknown";
+                    break;
+            }
+            return name + " " + statusDescription;
+        }
+    }
+}
